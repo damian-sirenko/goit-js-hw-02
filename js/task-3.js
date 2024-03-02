e9a982d80c2e8fe0d6f345794a3d8a21ccc062fd
damian-sirenko <damian.sirenko@gmail.com>
2024-03-02 14:09:55 +0100

    hw 2 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,21 @@
-function getElementWidth(content, padding, border) {
-	return (Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2)
+function checkForSpam(message){
+	const spamWord1 ='SALE';
+	const spamWord2 = 'SPAM';
+	const normalizedMessage = message.toUpperCase();
+	const hasSpamWords1 = normalizedMessage.includes(spamWord1);
+	const hasSpamWords2 = normalizedMessage.includes(spamWord2);
+	if (hasSpamWords1 || hasSpamWords2) {
+		return true;
+	}	else {
+		return false;
+	}
 }
+	
 
-
-// Results
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
